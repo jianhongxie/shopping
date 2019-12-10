a63306e62e4956956d246f277a4852ac8c6e299f
夜的钢琴曲 <867331891@qq.com>
2019-12-10 18:32:14 +0800

    add

---
+++ src/taobao/domain/RoamingMessageItem.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace Shopping\OpenSDK\TaoBao\Domain;
+
+/**
+ * 消息节点
+ * @author auto create
+ */
+class RoamingMessageItem
+{
+	
+	/** 
+	 * 节点类型
+	 **/
+	public $type;
+	
+	/** 
+	 * 值
+	 **/
+	public $value;	
+}
+?>
